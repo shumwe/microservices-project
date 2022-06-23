ac4f5522fd728b12b8aa6e87de8e6aab8494800e
Bravin Shumwe <bravinshumwe@gmail.com>
2022-06-23 09:46:55 +0000

    run_kubernetes.sh cmds

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,21 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="shumwe/predictorapi"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run predictorapipod \
+    --image=$dockerpath \
+    --port=80 \
+    --labels app=predictorapipod
+    --generator=run-pod/v1
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward predictorapipod 8000:80
