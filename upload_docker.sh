b154168d26a2649aa1aed6c7d60190e3580515eb
Bravin Shumwe <bravinshumwe@gmail.com>
2022-06-23 09:28:00 +0000

    upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="shumwe/predictorapi"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker image tag predictorapi $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
